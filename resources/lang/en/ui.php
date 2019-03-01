cb98b303d615c605483de7e0265b255787591b44
Yz. Chen <yanzhen610@gmail.com>
2019-03-02 03:21:04 +0800

    added some i18n msg

---
+++ resources/lang/en/ui.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'login' => 'Sign In',
+    'logout' => 'Sign Out',
+    'register' => 'Sign Up',
+];
---
+++ resources/lang/zh/ui.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'login' => '登入',
+    'logout' => '登出',
+    'register' => '註冊',
+    'home' => '首頁',
+    'user' => '帳戶',
+    'users' => '所有人員',
+    'duty' => '職務',
+    'building' => '大樓',
+    'area' => '區域',
+    'username' => '帳號',
+    'password' => '密碼',
+    'login_remember' => '保持登入狀態',
+    'forget_password' => '忘記密碼',
+    'display_name' => '名稱',
+    'is_admin' => '管理員',
+    'is_disabled' => '禁用',
+    'admin' => '管理員',
+    'disabled' => '禁用',
+    'save' => '保存',
+    'mobile_ext' => '手機/分機',
+    'create' => '新增',
+    'reset_password' => '重置密碼',
+];
