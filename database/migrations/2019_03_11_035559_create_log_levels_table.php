abd0a130a3e755de0801323aa4cc00dd269210cf
Yz. Chen <yanzhen610@gmail.com>
2019-04-07 14:38:32 +0800

    added log table

---
+++ database/migrations/2019_03_11_035559_create_log_levels_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogLevelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_levels', function (Blueprint $table) {
+            $table->increments('id')->index();
+
+            $table->string('level_name')->index();
+            $table->integer('level')->index();
+        });
+
+        DB::table('log_levels')->insert([
+            'level_name' => 'Info',
+            'level' => 1,
+        ]);
+
+        DB::table('log_levels')->insert([
+            'level_name' => 'Important',
+            'level' => 2,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_levels');
+    }
+}
---
+++ database/migrations/2019_03_11_035647_create_logs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->bigIncrements('id')->index();
+
+            $table->unsignedInteger('level')->index();
+            $table->foreign('level')->references('id')->on('log_levels')->onDelete('restrict')->onUpdate('cascade');
+
+            $table->unsignedInteger('tag')->index();
+            $table->foreign('tag')->references('id')->on('log_tags')->onDelete('restrict')->onUpdate('cascade');
+
+            $table->text('message');
+
+            $table->timestamps();
+            $table->index(['created_at', 'updated_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
