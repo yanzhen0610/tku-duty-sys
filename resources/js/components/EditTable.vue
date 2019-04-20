68278cd01af8369c500f48ecb67ae313a0cbc1a8
Yz. Chen <yanzhen610@gmail.com>
2019-04-20 13:24:03 +0800

    indentation

--- resources/js/components/EditTable.vue
+++ resources/js/components/EditTable.vue
@@ -1,48 +1,47 @@
 <template>
     <div>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>{{ i18n[primary_key] }}</th>
-                <th v-for="(_, key) in fields"
-                    v-bind:key="key"
-                >{{ i18n[key] }}</th>
-                <th v-if="editable"
-                >{{ i18n['save'] }}</th>
-                <th v-if="destroyable"
-                >{{ i18n['delete'] }}</th>
-            </tr>
-        </thead>
-        <tfoot>
-            <tr>
-                <th>{{ i18n[primary_key] }}</th>
-                <th v-for="(_, key) in fields"
-                    v-bind:key="key"
-                >{{ i18n[key] }}</th>
-                <th v-if="editable"
-                >{{ i18n['save'] }}</th>
-                <th v-if="destroyable"
-                >{{ i18n['delete'] }}</th>
-            </tr>
-        </tfoot>
-        <edit-table-row
-            v-for="(rowData, index) in rows"
-            v-bind:key="rowData.editTableRowIndex"
-            v-bind:rowKey="index"
-            v-bind:rowData="rowData"
-        ></edit-table-row>
-    </table>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>{{ i18n[primary_key] }}</th>
+                    <th v-for="(_, key) in fields"
+                        v-bind:key="key"
+                    >{{ i18n[key] }}</th>
+                    <th v-if="editable"
+                    >{{ i18n['save'] }}</th>
+                    <th v-if="destroyable"
+                    >{{ i18n['delete'] }}</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <th>{{ i18n[primary_key] }}</th>
+                    <th v-for="(_, key) in fields"
+                        v-bind:key="key"
+                    >{{ i18n[key] }}</th>
+                    <th v-if="editable"
+                    >{{ i18n['save'] }}</th>
+                    <th v-if="destroyable"
+                    >{{ i18n['delete'] }}</th>
+                </tr>
+            </tfoot>
+            <edit-table-row
+                v-for="(rowData, index) in rows"
+                v-bind:key="rowData.editTableRowIndex"
+                v-bind:rowKey="index"
+                v-bind:rowData="rowData"
+            ></edit-table-row>
+        </table>
 
-    <a class="button is-success is-outlined is-rounded"
-        v-if="create_url"
-        @click="createNewRow()"
-    >
-        <span class="icon is-small">
-            <i class="fas fa-plus"></i>
-        </span>
-        <span>{{ i18n['create'] }}</span>
-    </a>
-
+        <a class="button is-success is-outlined is-rounded"
+            v-if="create_url"
+            @click="createNewRow()"
+        >
+            <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span>{{ i18n['create'] }}</span>
+        </a>
     </div>
 </template>
 
