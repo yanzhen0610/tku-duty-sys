344f85c89754e45197e4b890e8589c3ba363bd34
Yz. Chen <yanzhen610@gmail.com>
2019-04-20 13:36:55 +0800

    updated password reset flow

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use App\Events\ResetPasswordRequested;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +31,32 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Display the form to request to reset a user's password.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showRequestResetForm()
+    {
+        return view('auth.passwords.requestReset');
+    }
+
+    public function requestReset(Request $request)
+    {
+        $request->validate([
+            'username' => ['required'],
+        ]);
+
+        if ($user = User::where('username', $request->input('username'))->first())
+        {
+            $user->status = User::$STATUS_RESET_PASSWORD_REQUESTED;
+            $user->save();
+        }
+
+        event(new ResetPasswordRequested($request->input('username')));
+
+        return back()->with('status', trans('passwords.reset_request_send_to_admin'));
     }
 }
