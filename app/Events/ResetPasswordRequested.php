9ddd52efdcb45ceca473fe0eac4c64eb2403952b
Yz. Chen <yanzhen610@gmail.com>
2019-04-20 13:37:25 +0800

    added reset password event

---
+++ app/Events/ResetPasswordRequested.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class ResetPasswordRequested
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $username;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(String $username)
+    {
+        $this->$username = $username;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
