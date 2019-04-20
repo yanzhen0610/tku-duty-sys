693ed6f16e7eaaf6fb0b05fe66d6d199d41394c3
Yz. Chen <yanzhen610@gmail.com>
2019-04-21 00:54:09 +0800

    it's not an example

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,18 +1,18 @@
 <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <a class="navbar-item" href="{{ route('home') }}">
-            <img src="https://photo.tku.edu.tw/getjpg.cshtml?im=4BF6E0F0441D74FA54F7801CBC27726D8EED46780AC64A531821B52AFA2CB4758592FECE924320F2B27AE0489CC89B1E95BBAF3CECE0C7CB" width="28" height="28">
+            <img src="{{ asset('images/logo.jpg') }}" width="28" height="28">
         </a>
 
         <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
-            data-target="navbarBasicExample">
+            data-target="navbar">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
         </a>
     </div>
 
-    <div id="navbarBasicExample" class="navbar-menu">
+    <div id="navbar" class="navbar-menu">
         <div class="navbar-start">
             <a class="navbar-item" href="{{ route('home') }}">
                 @lang('ui.home')
