98c24fe9bca460561efda2282ac7820a61f48839
Yz. Chen <yanzhen610@gmail.com>
2019-04-24 01:28:11 +0800

    added areas & shifts tables

---
+++ database/migrations/2019_04_23_172308_create_shifts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->bigIncrements('id')->index();
+            $table->uuid('uuid')->unique()->index();
+
+            $table->bigInteger('area_id')->unsigned();
+            $table->foreign('area_id')->references('id')->on('areas')
+                ->onDelete('restrict')->onUpdate('cascade');
+
+            $table->string('shift_name');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shifts');
+    }
+}
