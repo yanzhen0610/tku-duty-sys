f0517f7bc4471c42a4c042a7b22c5c2bea28bbb0
Yz. Chen <yanzhen610@gmail.com>
2019-05-07 23:09:18 +0800

    added model: area & shift

---
+++ app/Area.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Area extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'area_name',
+        'responsible_person_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['responsible_person'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function(Area $area) {
+            $area->uuid = Str::uuid()->toString();
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
+
+    public function getResponsiblePersonAttribute()
+    {
+        return $this->responsiblePerson()->getResults();
+    }
+
+    public function setResponsiblePersonAttribute($value)
+    {
+        if ($value instanceof User) {
+            $this->responsible_person_id = $value->id;
+            $this->save();
+        }
+        else if (is_int($value))
+        {
+            $this->responsible_person_id = $value;
+            $this->save();
+        }
+        else if (is_string($value))
+        {
+            $this->responsible_person_id = User::where('username', $value)->first()->id;
+            $this->save();
+        }
+    }
+
+    public function responsiblePerson()
+    {
+        return $this->belongsTo('App\User', 'responsible_person_id', 'id');
+    }
+}
---
+++ app/Shift.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+class Shift extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'shift_name',
+        'area_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['area'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function(Shift $shift) {
+            $shift->uuid = Str::uuid()->toString();
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
+
+    public function getAreaAttribute()
+    {
+        return $this->area()->getResults();
+    }
+
+    public function setAreaAttribute($value)
+    {
+        if ($value instanceof Area)
+        {
+            $this->area_id = $value->id;
+            $this->save();
+        }
+        else if (is_int($value))
+        {
+            $this->area_id = $value;
+            $this->save();
+        }
+        else if (is_string($value))
+        {
+            $this->area_id = Area::where('uuid', $value)->first()->id;
+            $this->save();
+        }
+    }
+
+    public function area()
+    {
+        return $this->belongsTo('App\Area', 'area_id', 'id');
+    }
+}
