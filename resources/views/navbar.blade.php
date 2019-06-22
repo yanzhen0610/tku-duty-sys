920279729ce4826638c6f8f66184c7d0cc34a653
Yz. Chen <yanzhen610@gmail.com>
2019-06-22 15:15:37 +0800

    added container to navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,93 +1,91 @@
 <nav class="navbar is-transparent has-shadow is-spaced" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <a class="navbar-item" href="{{ route('home') }}">
-            <img src="{{ asset('images/logo.jpg') }}" width="28" height="28">
-        </a>
-
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbar">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </a>
-    </div>
-
-    <div id="navbar" class="navbar-menu">
-        <div class="navbar-start">
-
-            <!-- <a class="navbar-item" href="{{ route('home') }}">
-                @lang('ui.home')
-            </a> -->
-
-            <a class="navbar-item" href="{{ route('pages.shifts_arrangements_table') }}">
-                @lang('ui.shifts_arrangements_table')
+    <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="{{ route('home') }}">
+                <img src="{{ asset('images/logo.jpg') }}" width="28" height="28">
             </a>
 
-            <a class="navbar-item" href="{{ route('pages.shifts_arrangements_table') }}">
-                @lang('ui.apply_for_a_shift_change')
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbar">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
             </a>
-
-            <a class="navbar-item" href="{{ route('pages.areas') }}">
-                @if (Auth::user()->is_admin)
-                    @lang('ui.areas_management')
-                @else
-                    @lang('ui.areas')
-                @endif
-            </a>
-
-            <a class="navbar-item" href="{{ route('pages.shifts') }}">
-                @if (Auth::user()->is_admin)
-                    @lang('ui.shifts_management')
-                @else
-                    @lang('ui.shifts')
-                @endif
-            </a>
-
-            <a class="navbar-item" href="{{ route('pages.users') }}">
-                @if (Auth::user()->is_admin)
-                    @lang('ui.users_management')
-                @else
-                    @lang('ui.users')
-                @endif
-            </a>
-
         </div>
 
-        @guest
-        <div class="navbar-end">
-            <div class="navbar-item">
-                <div class="buttons">
-                    <a class="button is-primary" href="{{ route('login') }}">
-                        @lang('ui.login')
-                    </a>
-                </div>
-            </div>
-        </div>
-        @else
-        <div class="navbar-end">
-            <div class="navbar-item has-dropdown is-hoverable" onclick="this.classList.toggle('is-active')">
-                <a class="navbar-link">
-                    {{ Auth::user()->display_name ?? Auth::user()->username }}
+        <div id="navbar" class="navbar-menu">
+            <div class="navbar-start">
+
+                <!-- <a class="navbar-item" href="{{ route('home') }}">
+                    @lang('ui.home')
+                </a> -->
+
+                <a class="navbar-item" href="{{ route('pages.shifts_arrangements_table') }}">
+                    @lang('ui.shifts_arrangements_table')
                 </a>
 
-                <div class="navbar-dropdown is-right is-boxed">
-                    <a class="navbar-item" href="{{ route('user.self') }}">
-                        @lang('ui.user')({{ Auth::user()->username }})
-                    </a>
+                <a class="navbar-item" href="{{ route('pages.areas') }}">
+                    @if (Auth::user()->is_admin)
+                        @lang('ui.areas_management')
+                    @else
+                        @lang('ui.areas')
+                    @endif
+                </a>
 
-                    <hr class="navbar-divider">
+                <a class="navbar-item" href="{{ route('pages.shifts') }}">
+                    @if (Auth::user()->is_admin)
+                        @lang('ui.shifts_management')
+                    @else
+                        @lang('ui.shifts')
+                    @endif
+                </a>
 
-                    <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                        @lang('ui.logout')
-                    </a>
+                <a class="navbar-item" href="{{ route('pages.users') }}">
+                    @if (Auth::user()->is_admin)
+                        @lang('ui.users_management')
+                    @else
+                        @lang('ui.users')
+                    @endif
+                </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
+            </div>
+
+            @guest
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary" href="{{ route('login') }}">
+                            @lang('ui.login')
+                        </a>
+                    </div>
                 </div>
             </div>
+            @else
+            <div class="navbar-end">
+                <div class="navbar-item has-dropdown is-hoverable" onclick="this.classList.toggle('is-active')">
+                    <a class="navbar-link">
+                        {{ Auth::user()->display_name ?? Auth::user()->username }}
+                    </a>
+
+                    <div class="navbar-dropdown is-right is-boxed">
+                        <a class="navbar-item" href="{{ route('user.self') }}">
+                            @lang('ui.user')({{ Auth::user()->username }})
+                        </a>
+
+                        <hr class="navbar-divider">
+
+                        <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                            @lang('ui.logout')
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+            </div>
+            @endguest
         </div>
-        @endguest
     </div>
 </nav>
 
